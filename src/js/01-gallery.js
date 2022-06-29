5e2fd69b59ee2fe9d08bac7c5a5461a82def4506
Igor432 <igomar0602@gmail.com>
2022-06-29 15:23:31 +0300

    baza2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+const img = galleryItems.map((image, index) =>   `<li class="gallery__item "><img class='gallery__image' src=${image.preview} alt = ${image.description}></li>`).join('')
+
+gallery.insertAdjacentHTML('afterbegin', img)
+
+
+
+
+gallery.addEventListener('click', modalOpen);
+
+function modalOpen(event) {
+
+ 
+
+    const target = galleryItems.map((image) => 
+    `<li><img class='item' src=${image.original}></li>`)
+ 
+  }
+
+
+  /*
+
+)
+
+  */
+
+
+
+ 
