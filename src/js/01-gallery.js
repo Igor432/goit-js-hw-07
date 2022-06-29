99914c253d78391ce20b71c9747e59dd0a8dc9e8
Igor432 <igomar0602@gmail.com>
2022-06-29 18:29:57 +0300

    baza 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,42 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
 
 
-const img = galleryItems.map((image, index) =>   `<li class="gallery__item "><img class='gallery__image' src=${image.preview} alt = ${image.description}></li>`).join('')
+const img = galleryItems.map((image, index) =>
+    `<li class="gallery__item "><img class='gallery__image' src=${image.preview} data-source=${image.original} alt = ${image.description}></li>`).join('')
 
 gallery.insertAdjacentHTML('afterbegin', img)
 
 
 
-
-gallery.addEventListener('click', modalOpen);
-
-function modalOpen(event) {
-
- 
-
-    const target = galleryItems.map((image) => 
-    `<li><img class='item' src=${image.original}></li>`)
- 
-  }
+gallery.addEventListener('click', openBig);
 
 
-  /*
+function openBig(event) {
+    event.preventDefault();
+    const imageItem = event.target.classList.contains('gallery__image')
+    const url = event.target.dataset.source;
 
-)
+    if (!imageItem) {
+        return 
+    }
 
-  */
+    const instance = basicLightbox.create(
+        `<img src = ${url}>`
+    );
+    
+    instance.show();
 
+    document.addEventListener('keydown', event =>{
+        if (event.key === 'Escape') {
+            instance.close();
+            
+        }
+        return;
+    });
 
+}
 
- 
